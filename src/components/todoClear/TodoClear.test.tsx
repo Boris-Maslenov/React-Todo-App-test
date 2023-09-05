a8e2f54a0be9c7d7ddabd93f0e158b97eee03702
Boris Maslenov <ya.maslenov@ya.ru>
2023-09-05 11:31:43 +0300

    added: Click Button test

--- src/components/todoClear/TodoClear.test.tsx
+++ src/components/todoClear/TodoClear.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import {TodoClear} from './';
 import { TodoState } from '../../context/TodoState';
 
@@ -6,5 +6,11 @@ describe("TodoClear Component", () => {
     test("render component", () => {
         const {getByRole} = render(<TodoState><TodoClear /></TodoState>);
         expect(getByRole('button')).toBeInTheDocument();
+    });
+    test("Click Button", async () => {
+        render(<TodoState><TodoClear /></TodoState>)
+        expect(screen.getByRole('button')).toBeEnabled();
+        fireEvent.click(screen.getByRole('button'), {})
+        expect( await screen.findByRole('button')).not.toBeEnabled();
     });
 })
