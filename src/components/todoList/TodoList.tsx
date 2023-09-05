2211119b68f1bd47497f2af386b9575e904a8c5f
Boris Maslenov <ya.maslenov@ya.ru>
2023-09-05 17:37:38 +0300

    raised the logic to the this component

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -4,13 +4,51 @@ import { TodoContext } from '../../context/TodoContext';
 import { TodoItemBase, TodoItemEdit } from '../todoItem/';
 
 export const TodoList = () => {
+    const { dispatch } = useContext(TodoContext);
     const [count, setCount] = useState(0);
     const { state } = useContext(TodoContext);
+    // Fix:
     useEffect(()=>{
         const activeTodos = state.todos.filter(({completed})=> !completed);
         const countActiveTodos = activeTodos.length;
         setCount(countActiveTodos);
     },[state]);
+
+    const handleComplete = (id: string) => {
+        dispatch({ 
+            type: 'TODO_COMPLETE',
+            payload: id
+        })  
+    }
+
+    const handleRemove = (id: string) => {
+        dispatch({
+            type: 'TODO_REMOVE',
+            payload: id
+        })
+    }
+
+    const handleEdit = (id: string) => {
+        dispatch({
+            type: 'TODO_EDIT',
+            payload: id
+        })
+    }
+
+    const handleUpdate = (id:string, value:string) => {
+        if (value.trim()){
+            // Обновляем данные
+            dispatch({
+                type: "TODO_UPDATE",
+                payload: { id, value },
+            });
+        }
+        dispatch({
+            type: "TODO_EDIT",
+            payload:  id ,
+        });
+      };
+
     return (
         <Container>
             <h3>{count ? `Todos free: ${count}` : 'No todos free'}</h3>
@@ -27,8 +65,8 @@ export const TodoList = () => {
                           >
                             {
                                 !todo.edit ? 
-                                    <TodoItemBase {...todo} /> :
-                                    <TodoItemEdit {...todo} />                                 
+                                    <TodoItemBase {...todo } handleComplete={handleComplete}  handleRemove={handleRemove} handleEdit={handleEdit} /> :
+                                    <TodoItemEdit {...todo} handleUpdate={handleUpdate} handleEdit={handleEdit} />                                 
                             }
                             
                         </ListGroup.Item>
