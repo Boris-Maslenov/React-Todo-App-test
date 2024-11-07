0c38a72867e7b247a12f25c3c3d09a3c2ce7eda4
maslenov_ba <maslenov_ba@teleformis.ru>
2024-11-07 21:22:21 +0300

    fix recomendeds

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,78 +1,84 @@
-import { useContext, useState, useEffect } from 'react';
-import { Container, ListGroup } from 'react-bootstrap';
-import { TodoContext } from '../../context/TodoContext';
-import { TodoItemBase, TodoItemEdit } from './todoItem';
+import { useContext, useCallback } from "react";
+import { Container, ListGroup } from "react-bootstrap";
+import { TodoContext } from "../../context/TodoContext";
+import { TodoItemBase, TodoItemEdit } from "./todoItem";
 
 export const TodoList = () => {
-    const { dispatch, state } = useContext(TodoContext);
-    const [count, setCount] = useState(0);
-    // Fix:
-    useEffect(()=>{
-        const activeTodos = state.todos.filter(({completed})=> !completed);
-        const countActiveTodos = activeTodos.length;
-        setCount(countActiveTodos);
-    },[state]);
+  const { dispatch, state } = useContext(TodoContext);
+  const freeCount = state.todos.filter(({ completed }) => !completed).length;
 
-    const handleComplete = (id: string) => {
-        dispatch({ 
-            type: 'todo/complete',
-            payload: id
-        })  
+  const handleComplete = useCallback((id: string) => {
+    dispatch({
+      type: "todo/complete",
+      payload: id,
+    });
+  }, []);
+
+  const handleRemove = (id: string) => {
+    dispatch({
+      type: "todo/remove",
+      payload: id,
+    });
+  };
+
+  const handleEdit = useCallback((id: string) => {
+    dispatch({
+      type: "todo/edit",
+      payload: id,
+    });
+  }, []);
+
+  const handleUpdate = useCallback((id: string, value: string) => {
+    if (value.trim()) {
+      dispatch({
+        type: "todo/update",
+        payload: { id, value },
+      });
     }
 
-    const handleRemove = (id: string) => {
-        dispatch({
-            type: 'todo/remove',
-            payload: id
-        })
-    }
+    dispatch({
+      type: "todo/edit",
+      payload: id,
+    });
+  }, []);
 
-    const handleEdit = (id: string) => {
-        dispatch({
-            type: 'todo/edit',
-            payload: id
-        })
-    }
-
-    const handleUpdate = (id:string, value:string) => {
-        if (value.trim()){
-            // Обновляем данные
-            dispatch({
-                type: "todo/update",
-                payload: { id, value },
-            });
-        }
-        dispatch({
-            type: "todo/edit",
-            payload:  id ,
-        });
-      };
-
-    return (
-        <Container>
-            <h3>{count ? `Todos free: ${count}` : 'No todos free'}</h3>
-            <ListGroup role="list">
-                {
-                    state.todos.filter(({completed})=> state.activeFilter === "all" ? true :
-                        state.activeFilter === "completed" ?
-                        completed :
-                        !completed)
-                       .map((todo) => todo.show && (
-                        <ListGroup.Item  
-                            className='d-flex align-items-center'
-                            key={todo.id}
-                          >
-                            {
-                                !todo.edit ? 
-                                    <TodoItemBase {...todo } handleComplete={handleComplete}  handleRemove={handleRemove} handleEdit={handleEdit} /> :
-                                    <TodoItemEdit {...todo} handleUpdate={handleUpdate} handleEdit={handleEdit} />                                 
-                            }
-                            
-                        </ListGroup.Item>
-                        )
-                    )     
-                }
-            </ListGroup>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <h3>{freeCount > 0 ? `Todos free: ${freeCount}` : "No todos free"}</h3>
+      <ListGroup role="list">
+        {state.todos
+          .filter(({ completed }) =>
+            state.activeFilter === "all"
+              ? true
+              : state.activeFilter === "completed"
+              ? completed
+              : !completed
+          )
+          .map(
+            (todo) =>
+              todo.show && (
+                <ListGroup.Item
+                  className="d-flex align-items-center"
+                  key={todo.id}
+                >
+                  {!todo.edit ? (
+                    <TodoItemBase
+                      {...todo}
+                      handleComplete={handleComplete}
+                      handleRemove={handleRemove}
+                      handleEdit={handleEdit}
+                    />
+                  ) : (
+                    <TodoItemEdit
+                      {...todo}
+                      handleUpdate={handleUpdate}
+                      handleEdit={handleEdit}
+                    />
+                  )}
+                </ListGroup.Item>
+              )
+          )}
+      </ListGroup>
+    </Container>
+  );
+};
